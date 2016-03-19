00ac698fee12fdf012fc14b3467f81dfcfd844e3
volvet <volvet2002@gmail.com>
2016-03-19 19:12:48 +0800

    initialize draw3d

---
+++ Draw3d/Draw3d/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Draw3d
+//
+//  Created by Volvet Zhang on 16/3/19.
+//  Copyright © 2016年 volvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
