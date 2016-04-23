7924f4a2a978631e9b8af7087b29b98baab9cca1
volvet <qizh@cisco.com>
2016-04-23 15:24:33 +0800

    metal image processing demo

---
+++ MetalImage/MetalImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetalImage
+//
+//  Created by Volvet Zhang on 16/4/23.
+//  Copyright © 2016年 volvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetalImage/MetalImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetalImage
+//
+//  Created by Volvet Zhang on 16/4/23.
+//  Copyright © 2016年 volvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
