93cc8ee26a4b0e3d8d9ba867a6751151e7e81897
volvet <qizh@cisco.com>
2016-05-01 09:42:02 +0800

    guassion filter

--- MetalImage/MetalImage/GaussianBlurFilter.m
+++ MetalImage/MetalImage/GaussianBlurFilter.m
@@ -29,4 +29,62 @@
     return self;
 }
 
+- (void) generateBlurWeightTexture {
+    NSAssert(_radius > 0.0f, @"Blur radius must > 0");
+    
+    const float radius = _radius;
+    const float sigma = _sigma;
+    const int  size = (round(radius) * 2 + 1);
+    float delta = 0;
+    float expScale = 0;
+    
+    if( radius > 0.0f ){
+        delta = (radius * 2)/(size - 1);
+        expScale = -1 / (2 * sigma * sigma);
+    }
+    
+    float * weights = malloc(sizeof(float) * size * size);
+    float weightSum = 0;
+    float y = - radius;
+    for( int j=0;j<size; ++j, y += delta) {
+        float x = - radius;
+        for( int i=0;i<size; ++i, x += delta) {
+            float weight = expf((x*x + y*y) * expScale);
+            weights[j*size + i] = weight;
+            weightSum += weight;
+        }
+    }
+    const float weightScale = 1/weightSum;
+    for( int i=0;i<size * size;i++ ){
+        weights[i] *= weightScale;
+    }
+    
+    MTLTextureDescriptor * descriptor = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatR32Float width:size height:size mipmapped:NO];
+    _blurWeightTexture = [self.context.device newTextureWithDescriptor :descriptor];
+    
+    MTLRegion region = MTLRegionMake2D(0, 0, size, size);
+    [_blurWeightTexture replaceRegion:region mipmapLevel:0 withBytes:weights bytesPerRow:sizeof(float)*size];
+    
+    free(weights);
+}
+
+- (void)setRadius:(float)radius {
+    self.dirty = true;
+    _radius = radius;
+    _blurWeightTexture = nil;
+}
+
+- (void)setSigma:(float)sigma {
+    self.dirty = true;
+    _sigma = sigma;
+    _blurWeightTexture = nil;
+}
+
+- (void)configureArgumentTableWithCommandEncoder:(id<MTLComputeCommandEncoder>)commandEncoder {
+    if( !_blurWeightTexture ) {
+        [self generateBlurWeightTexture];
+    }
+    [commandEncoder setTexture:_blurWeightTexture atIndex:2];
+}
+
 @end
