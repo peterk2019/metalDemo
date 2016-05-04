9804f015ce817322da4cf1dde1393da52329b883
volvet <qizh@cisco.com>
2016-05-04 21:45:38 +0800

    add image for processing

--- MetalImage/MetalImage/ViewController.h
+++ MetalImage/MetalImage/ViewController.h
@@ -10,6 +10,16 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (weak, nonatomic) IBOutlet UISlider *blurRadiusSlider;
+
+@property (weak, nonatomic) IBOutlet UISlider *saturationSlider;
+
+- (IBAction)blurRadiusDidChange:(id)sender;
+
+
+- (IBAction)saturationDidChange:(id)sender;
 
 @end
 
--- MetalImage/MetalImage/ViewController.m
+++ MetalImage/MetalImage/ViewController.m
@@ -7,8 +7,23 @@
 //
 
 #import "ViewController.h"
+#import "IPContext.h"
+#import "ImageFilter.h"
+#import "GaussianBlurFilter.h"
+#import "SaturationAdjustmentFilter.h"
+#import "UIImage+TextureUtility.h"
+#import "MainBoundleTextureProvider.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong)  IPContext *   context;
+@property (nonatomic, strong)  id<TextureProvider>    imageProvider;
+@property (nonatomic, strong)  SaturationAdjustmentFilter  *  saturationFilter;
+@property (nonatomic, strong)  GaussianBlurFilter * gaussionBlurFilter;
+
+@property (nonatomic, strong)  dispatch_queue_t   renderingQueue;
+@property (atomic, assign)     uint64_t           jobIndex;
+
 
 @end
 
@@ -17,6 +32,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _renderingQueue = dispatch_queue_create("Rendering", DISPATCH_QUEUE_SERIAL);
+    [self buildFilterGraph];
+    //[self updateImage];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +43,42 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)buildFilterGraph {
+    _context = [IPContext newContext];
+    _imageProvider = [MainBoundleTextureProvider textureProviderWithImageNamed:@"mandrill" context:_context];
+    //_saturationFilter = [SaturationAdjustmentFilter filterWithSaturationFactor:self.saturationSlider.value context:_context];
+    //_saturationFilter.provider = _imageProvider;
+    //_gaussionBlurFilter = [GaussianBlurFilter filterWithRadius:self.blurRadiusSlider.value :_context];
+    //_gaussionBlurFilter.provider = _imageProvider;
+    _imageView.image = [UIImage imageWithMTLTexture:_imageProvider.texture];
+}
+
+- (void)updateImage {
+    ++ _jobIndex;
+    
+    uint64_t currentJobIndex = _jobIndex;
+    float blurRadius = _blurRadiusSlider.value;
+    float saturation = _saturationSlider.value;
+    
+    dispatch_sync(_renderingQueue, ^{
+        if( currentJobIndex != _jobIndex ) return;
+        _gaussionBlurFilter.radius = blurRadius;
+        _saturationFilter.saturationFactor = saturation;
+        
+        id<MTLTexture>  texture = _imageProvider.texture; //_gaussionBlurFilter.texture;
+        UIImage * image = [UIImage imageWithMTLTexture:texture];
+        
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            _imageView.image = image;
+        });
+    });
+}
+
+- (IBAction)blurRadiusDidChange:(id)sender {
+    //[self updateImage];
+}
+
+- (IBAction)saturationDidChange:(id)sender {
+    //[self updateImage];
+}
 @end
